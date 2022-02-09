0b6de86dc3142ae4ab767cb01aa1eea662787c02
Raokk <59ichiigo@gmail.com>
2022-02-09 18:35:56 +0100

    add map class including parser inside

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
-use std::io;
+use crate::map::Map;
 
-macro_rules! parse_input {
-    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
-}
-
-pub fn parser() {
-    let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).unwrap();
-    let inputs = input_line.split(" ").collect::<Vec<_>>();
-    let w = parse_input!(inputs[0], i32);
-    let h = parse_input!(inputs[1], i32);
-    for i in 0..h as usize {
-        let mut input_line = String::new();
-        io::stdin().read_line(&mut input_line).unwrap();
-        let line = input_line.trim_end().to_string();
-        println!("{}", line);
-
-    }
-}
+mod map;
 
 fn main() {
-parser();
+    Map::parser();
     
 }
