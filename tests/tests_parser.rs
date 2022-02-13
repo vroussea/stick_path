f998c89dee25d8f98ef99ec3e083415a39d61c5e
Raokk <59ichiigo@gmail.com>
2022-02-13 20:21:32 +0100

    add the first tests on the parser checking if the diagram is well copied and converted

--- tests/tests_parser.rs
+++ tests/tests_parser.rs
@@ -8,4 +8,46 @@ mod tests_map {
         let vec = convert_to_cells(line);
         assert_eq!(vec[0], map::Cell{left: false, right: false, cell_char: '|'});
     }
+
+    fn convert_three_characters_line() {
+        let line: &str = "| | |";
+        let vec = convert_to_cells(line);
+        assert_eq!(vec[0], map::Cell{left: false, right: false, cell_char: '|'});
+        assert_eq!(vec[1], map::Cell{left: false, right: false, cell_char: '|'});
+        assert_eq!(vec[2], map::Cell{left: false, right: false, cell_char: '|'});
+    }
+
+    fn convert_one_bridge() {
+        let line: &str = "| |--|";
+        let vec = convert_to_cells(line);
+        assert_eq!(vec[0], map::Cell{left: false, right: false, cell_char: '|'});
+        assert_eq!(vec[1], map::Cell{left: false, right: true, cell_char: '|'});
+        assert_eq!(vec[2], map::Cell{left: true, right: false, cell_char: '|'});
+    }
+
+    fn convert_two_bridges() {
+        let line: &str = "|--|--|";
+        let vec = convert_to_cells(line);
+        assert_eq!(vec[0], map::Cell{left: false, right: true, cell_char: '|'});
+        assert_eq!(vec[1], map::Cell{left: true, right: true, cell_char: '|'});
+        assert_eq!(vec[2], map::Cell{left: true, right: false, cell_char: '|'});
+    }
+
+    fn first_line() {
+        let line: &str = "? A n ,";
+        let vec = convert_to_cells(line);
+        assert_eq!(vec[0], map::Cell{left: false, right: false, cell_char: '?'});
+        assert_eq!(vec[1], map::Cell{left: false, right: false, cell_char: 'A'});
+        assert_eq!(vec[2], map::Cell{left: false, right: false, cell_char: 'n'});
+        assert_eq!(vec[3], map::Cell{left: false, right: false, cell_char: ','});
+    }
+
+    fn last_line() {
+        let line: &str = "1 A $ ,";
+        let vec = convert_to_cells(line);
+        assert_eq!(vec[0], map::Cell{left: false, right: false, cell_char: '1'});
+        assert_eq!(vec[1], map::Cell{left: false, right: false, cell_char: 'A'});
+        assert_eq!(vec[2], map::Cell{left: false, right: false, cell_char: '$'});
+        assert_eq!(vec[3], map::Cell{left: false, right: false, cell_char: ','});
+    }
 }
