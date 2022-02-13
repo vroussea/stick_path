6dac349d0fa9379efe02b95d1c0137c3f7b23f48
Raokk <59ichiigo@gmail.com>
2022-02-13 20:12:45 +0100

    fix tests

--- tests/tests_parser.rs
+++ tests/tests_parser.rs
@@ -1,11 +1,11 @@
-use super::map::Cell;
-
 #[cfg(test)]
 mod tests_map {
+    use stick_path::functions::map;
+
     #[test]
     fn convert_simple_line() {
         let line: &str = "|";
         let vec = convert_to_cells(line);
-        assert_eq!(vec[0], Cell{left: false, right: false, cell_char: '|'});
+        assert_eq!(vec[0], map::Cell{left: false, right: false, cell_char: '|'});
     }
 }
