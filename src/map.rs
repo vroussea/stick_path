5caa700743b51d2bdf18944763f0cb7d0b25c453
Raokk <59ichiigo@gmail.com>
2022-02-13 19:08:39 +0100

    separate the code in smaller pieces

--- src/map.rs
+++ src/map.rs
@@ -5,25 +5,37 @@ macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>()?)
 }
 
-struct Connector {
+struct Cell {
     left: bool,
     right: bool,
+    cell_char: char,
 }
 
 pub struct Map {
-    height: i8,
-    width: i8,
-    map: Vec<Vec<Connector>>,
+    height: u8,
+    width: u8,
+    map: Vec<Vec<Cell>>,
+}
+
+fn read_line() -> Result<String, errors::CustomError>{
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line)?;
+    return Ok(input_line);
+}
+
+fn get_size() -> Result<(u8, u8), errors::CustomError> {
+    let input_line = read_line()?;
+    let inputs = input_line.split(" ").collect::<Vec<_>>();
+    let width = parse_input!(inputs[0], u8);
+    let height = parse_input!(inputs[1], u8);
+    return Ok((width, height));
 }
 
 impl Map {
-    pub fn parser() -> Result<Map, errors::CustomError>{
-        let mut input_line = String::new();
-        io::stdin().read_line(&mut input_line)?;
-        let inputs = input_line.split(" ").collect::<Vec<_>>();
-        let w = parse_input!(inputs[0], i32);
-        let h = parse_input!(inputs[1], i32);
-        for i in 0..h as usize {
+    pub fn parser() -> Result<Map, errors::CustomError> {
+        let (width, height) = get_size()?;
+        let map = Map::new(height, width);
+        for i in 0..height as usize {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line)?;
             let line = input_line.trim_end().to_string();
@@ -31,5 +43,13 @@ impl Map {
         }
         
         return Ok(Map {height: 1, width: 2, map: Vec::new()});
+    }
+
+    fn new(height: u8, width: u8) -> Map {
+        return Map {
+            height: height,
+            width: width,
+            map: Vec::new(),
+        };
     }
 }
