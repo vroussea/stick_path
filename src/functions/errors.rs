738390300197b6306c7a222c8b414c09f35ce079
Raokk <59ichiigo@gmail.com>
2022-03-09 19:59:19 +0100

    indent error

--- src/functions/errors.rs
+++ src/functions/errors.rs
@@ -7,18 +7,24 @@ pub struct CustomError {
 
 impl From<Error> for CustomError {
     fn from(e: Error) -> CustomError {
-        CustomError{message: format!("Generic IO error: {}", e.to_string())}
+        CustomError {
+            message: format!("Generic IO error: {}", e.to_string()),
+        }
     }
 }
 
 impl From<std::num::ParseIntError> for CustomError {
     fn from(e: std::num::ParseIntError) -> CustomError {
-        CustomError{message: format!("Invalid data type: {}", e.to_string())}
+        CustomError {
+            message: format!("Invalid data type: {}", e.to_string()),
+        }
     }
 }
 
 impl From<&str> for CustomError {
     fn from(e: &str) -> CustomError {
-        CustomError{message: e.to_string()}
+        CustomError {
+            message: e.to_string(),
+        }
     }
 }
